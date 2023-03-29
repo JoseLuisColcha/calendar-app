6071827a22ab73501fdc6efe876cb1561460d5cd
JoseLuisColcha <jose.colcha@epn.edu.ec>
2023-03-28 21:07:57 -0500

    First commit

---
+++ src/calendar/CaledarPage.js
@@ -0,0 +1,4 @@
+
+export const CaledarPage = () => {
+	return <div>CaledarPage</div>
+}
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { LoginPage } from '../auth/LoginPage'
+import { CaledarPage } from '../calendar/CaledarPage'
+
+export const AppRouter = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/login' element={<LoginPage />} />
+
+				<Route path='/' element={<CaledarPage />} />
+			</Routes>
+		</BrowserRouter>
+	)
+}
